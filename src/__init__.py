5558b9c89d5e81ccd5b0a61d66aeb6db69f14440
Starunov Andrei <ASStarunov@sberbank.ru>
2024-03-20 16:59:43 +0300

    feature: - reformat

--- src/__init__.py
+++ src/__init__.py
@@ -66,7 +66,7 @@ app.register_routes([
                 "description": "Всех данных сайта", "methods": ['GET'],
                 "endpoint": site_service.get_site_data
             },
-{
+            {
                 "path": "/list/{category}",
                 "responses": {400: {"description": "Bad request"}},
                 "description": "Получение списка сайтов по категории", "methods": ['GET'],
